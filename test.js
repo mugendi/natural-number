04eb9577437952391923d0c5b5306b1ab70b5a24
Anthony Mugendi <ngurumugz@gmail.com>
2015-07-19 15:35:43 +0300

    natural_number Auto-commit

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
 
-console.log('pushing to git')
-
-
-//push to git
-require('shelljs/global');
-
-var cmd ='git status && git add -A && git commit -m "natural_number Auto-commit" && git push -u --all';
-
-exec(cmd, function(code, output) {
-  // console.log('Exit code:', code);
-  console.log('Program output:', output);
-});
-
+console.log('pushing to git');
+require('./git-publish.js');
 
 
 
@@ -24,5 +13,7 @@ var natural_number=new natN({
 var string= 'there are two hundred and fifty two cowna and fourteen goats';
 
 natural_number.parse(string)
+
+
 
 

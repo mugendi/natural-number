22aca08e63a07dd730d6472d60997bdf43729ada
Anthony Mugendi <ngurumugz@gmail.com>
2015-07-19 16:19:04 +0300

    natural_number Auto-commit

--- git-publish.js
+++ git-publish.js
@@ -4,10 +4,10 @@ var shell = require('shelljs');
 var cmd ='git status && git add -A && git commit -m "natural_number Auto-commit" && git push -u --all';
 
 
-console.log("\n\n pushing to git");
+console.log("\n\n pushing to git....");
 
 shell.exec(cmd, {silent:true}, function(code, output) {
   // console.log('Exit code:', code);
-  console.log('Program output:', output);
+  console.log("DONE!\n\n" , output);
 });
 

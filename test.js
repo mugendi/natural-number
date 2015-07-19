5a9fcc10c5b0cbd53c9ddf3d73c8df2d444c849d
Anthony Mugendi <ngurumugz@gmail.com>
2015-07-19 16:22:52 +0300

    natural_number Auto-commit

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var natural_number=new natN({
   lang:'en'
 })
 
-var string= 'there are two hundred and fifty two cowna and fourteen goats. Negative twenty 4. She was the fifty second patient. What! Ksh1000!';
+var string= 'there are two hundred and fifty two cowna and fourteen goats. Negative twenty 4. She was the fifty second patient. What! Ksh1000! He was ranked 2nd';
 
 natural_number.parse(string)
 
